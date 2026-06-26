c76cc6e22ea49d2911727ab3508166fb8df3428b
useless-coder404 <shawon12hossain@gmail.com>
2026-06-27 02:09:45 +0600

    Add login controller with JWT generation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,13 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const signup = async (req, res) => {
     try {
         const {name, email, password} = req.body;
 
         if (!name || !email || !password) {
-            return res.status(400).json({ message: 'Name, email or password is missing.'})
+            return res.status(400).json({ message: 'Name, email or password is missing.'});
         } 
 
         const existingUser = await User.findOne({ email });
@@ -33,4 +34,40 @@ const signup = async (req, res) => {
     }
 }
 
-module.exports = { signup } 
+const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Either email or password is missing.'});
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials'});
+        } 
+
+        const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET, { expiresIn: '7d'});
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            createdAt: user.createdAt,
+            token,
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error'});
+    }
+}
+
+module.exports = { signup, login } 
