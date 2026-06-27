33802447199283a9e8ef912daf4ddf10db715b63
useless-coder404 <shawon12hossain@gmail.com>
2026-06-27 20:14:32 +0600

    Add auth middleware and protected /me route

--- controllers/authController.js
+++ controllers/authController.js
@@ -70,4 +70,23 @@ const login = async (req, res) => {
     }
 }
 
-module.exports = { signup, login } 
+const getMe = async (req, res) => {
+    try {
+        const userId = req.user.id
+        
+        const user = await User.findById(userId).select('-password');
+        
+        if (!user) {
+            return res.status(404).json({ message: 'User not found'});
+        }
+        
+        res.status(200).json(user)
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error'});
+    }
+
+}
+
+module.exports = { signup, login, getMe }; 
---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const protect = async (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token || !token.startsWith('Bearer')) {
+        return res.status(401).json({ message: 'No token, authorization denied'});   
+    }
+
+    const jwtToken = token.split(' ')[1];
+
+    try {
+        const decode = jwt.verify(jwtToken, process.env.JWT_SECRET);
+        req.user = decode
+        next()
+
+    } catch (error) {
+        console.error(error);
+        return res.status(401).json({ message: 'No token, authorization denied'});
+    }
+}
+
+module.exports = { protect };
