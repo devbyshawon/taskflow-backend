9bba85f082c5f782aba8779668a5798e46412eee
useless-coder404 <shawon12hossain@gmail.com>
2026-07-06 05:16:58 +0600

    Restore getMe route

--- controllers/authController.js
+++ controllers/authController.js
@@ -70,4 +70,17 @@ const login = async (req, res) => {
     }
 }
 
-module.exports = { signup, login }; 
+const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+module.exports = { signup, login, getMe }; 
